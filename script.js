989d57a12791762b827694e58636e43364c36ff3
PloKoon68 <ors_mehmet68@hotmail.com>
2025-11-09 17:57:29 +0300

    pivot added

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const cwidth = canvas.width
 
 const ctx = canvas.getContext('2d');   //for drawing objects in canvas
 
+
+//p prefix means percentage, instead of raw pixels
 const pHeight = (percentage) => {
     return percentage * cheight / 100;
 }
@@ -11,14 +13,34 @@ const pWidth = (percentage) => {
     return percentage * cwidth / 100;
 }
 
-const pfillRectWith = (x, w, y, h) => {
+const pfillRectWith = (x, w, y, h, color) => {
+    ctx.fillStyle = color;
     ctx.fillRect(pWidth(x), pHeight(y), pWidth(w), pHeight(h));
 }
 
-// Draw ground
-ctx.fillStyle = '#0d8a41ff';
-pfillRectWith(0, 100, 80, 20)
 
+const pdrawShape = (coordinates, color) => {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(pWidth(coordinates[0][0]), pHeight(coordinates[0][1])); 
+    for(let i = 1; i < coordinates.length; i++) {
+        ctx.lineTo(pWidth(coordinates[i][0]), pHeight(coordinates[i][1]));
+    }
+    ctx.closePath();
+    ctx.fill();  
+}
+
+// Draw ground
+pfillRectWith(0, 100, 80, 20, '#0d8a41ff')
+
+//draw pivot triangle
+pdrawShape([[50, 70], [45, 80], [55, 80]], '#5d6767ff');
+
+
+const PLANK_LENGTH = 400; 
+const PLANK_HEIGHT = 20; 
+const CENTER_X = canvas.width / 2; 
+const CENTER_Y = canvas.height / 2 + 50;
 
 
 /*
